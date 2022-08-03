884210108181fdde3c4cacc0a13ad49692eceb8b
DIONNIDES <dimakurgan123789@gmail.com>
2022-08-03 18:57:46 +0300

    1-getRequest+Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from './Components/Button';
+
+
+type GetType = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    const [get, setGet] = useState([]);
+    const  getRequestHandler = () => {
+        setGet([])
+    }
+
+    useEffect(()=>{fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => setGet(json))},[])
+
+    return (
+        <div className="App">
+
+            <Button nickName={'Clean'} callback={getRequestHandler}/>
+            <p></p>
+            <ul>
+            {get.map((el:GetType) =>{
+                return(<li>
+                    <span>{el.id}</span>
+                    <span>{el.userId}</span>
+                    <span>{el.title}</span>
+                </li>)
+            })}
+            </ul>
+            {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +48,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header>*/}
+        </div>
+    );
 }
 
 export default App;
